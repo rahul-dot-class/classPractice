5a253d310519bbd6a36c9f32a486c2c80ad425d6
rahul-dot-class <rahul07969@gmail.com>
2022-09-11 16:38:38 +0530

    commit 2

--- practice1.java
+++ practice1.java
@@ -5,6 +5,7 @@ public static void main(String [] args){
 System.err.println("Namaste");
 System.err.println("Namaste");
 System.err.println("Namaste");
+System.err.println("Namaste");
 int i=0;
 do{
     System.err.println("Pranam ");
